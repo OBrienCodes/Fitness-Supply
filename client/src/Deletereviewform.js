87a8e3889b87f55daf6902347cabfed0c3c6876b
Matthew O'Brien <matthew.kevin.obrien@gmail.com>
2021-09-11 18:20:57 -0400

    working on final delete fetch

--- client/src/Deletereviewform.js
+++ client/src/Deletereviewform.js
@@ -1,7 +1,45 @@
-function Deletereview() {
+import React, {useState} from "react";
+
+function DeleteReview({workout}){
+    const [createReview, setCreateReview] = useState(null)
+    const [inputRating, setInputRating] = useState(null)
+
+function NewReview(workout,createReview,inputRating) {
+    fetch("/reviews",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+            user_id: 1,
+            workout_id: workout.id,
+            comment: createReview,
+            rating: inputRating
+    })
+})
+    .then((resp) => resp.json())
+    .then((reviewsarray) => {
+      console.log(reviewsarray);
+})
+}
+function handleSubmit(e){
+    e.preventDefault()
+    NewReview(workout,createReview,inputRating)
+}
+    
     return(
-        <div>HI :)</div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Leave Your Review</label>
+                <input type='text' onChange={(e) => setCreateReview(e.target.value)}/>
+                <br></br>
+                <label>Rating</label>
+                <input type='text' onChange={(e) => setInputRating(e.target.value)}/>
+                <br></br>
+                <button type="submit">Submit Review</button>
+            </div>
+        </form>
     )
 }
 
-export default Deletereview;
+export default DeleteReview;
